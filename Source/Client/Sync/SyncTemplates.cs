60373697ad3ce2e5e18db1dfcafa2c84d94fc793
notfood <notfood@users.noreply.github.com>
2019-06-19 19:00:51 -0500

    API: Increase max arguments for SyncMethods

--- Source/Client/Sync/SyncTemplates.cs
+++ Source/Client/Sync/SyncTemplates.cs
@@ -25,5 +25,9 @@ namespace Multiplayer.Client
         static bool Prefix_3(MethodBase __originalMethod, object __instance, object __0, object __1, object __2) => General(__originalMethod, __instance, new[] { __0, __1, __2 });
         static bool Prefix_4(MethodBase __originalMethod, object __instance, object __0, object __1, object __2, object __3) => General(__originalMethod, __instance, new[] { __0, __1, __2, __3 });
         static bool Prefix_5(MethodBase __originalMethod, object __instance, object __0, object __1, object __2, object __3, object __4) => General(__originalMethod, __instance, new[] { __0, __1, __2, __3, __4 });
+        static bool Prefix_6(MethodBase __originalMethod, object __instance, object __0, object __1, object __2, object __3, object __4, object __5) => General(__originalMethod, __instance, new[] { __0, __1, __2, __3, __4, __5 });
+        static bool Prefix_7(MethodBase __originalMethod, object __instance, object __0, object __1, object __2, object __3, object __4, object __5, object __6) => General(__originalMethod, __instance, new[] { __0, __1, __2, __3, __4, __5, __6 });
+        static bool Prefix_8(MethodBase __originalMethod, object __instance, object __0, object __1, object __2, object __3, object __4, object __5, object __6, object __7) => General(__originalMethod, __instance, new[] { __0, __1, __2, __3, __4, __5, __6, __7 });
+        static bool Prefix_9(MethodBase __originalMethod, object __instance, object __0, object __1, object __2, object __3, object __4, object __5, object __6, object __7, object __8) => General(__originalMethod, __instance, new[] { __0, __1, __2, __3, __4, __5, __6, __7, __8 });
     }
 }
