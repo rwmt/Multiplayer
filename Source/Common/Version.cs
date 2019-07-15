60151dc208936ec1672307742d766a1682dba563
Parexy <46968966+Parexy@users.noreply.github.com>
2019-07-15 15:56:02 +0200

    Version MP 0.4.8.3, MPAPI 0.1, Protocol 17

--- Source/Common/Version.cs
+++ Source/Common/Version.cs
@@ -2,7 +2,7 @@ namespace Multiplayer.Common
 {
     public static class MpVersion
     {
-        public const string Version = "0.4.8";
+        public const string Version = "0.4.8.3";
         public const int Protocol = 17;
 
         public const string apiAssemblyName = "0MultiplayerAPI";
