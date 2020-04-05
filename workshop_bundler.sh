635f77e5d3efe0be991c1f7ef94d409169aab004
Nebual <ben1120@gmail.com>
2020-04-05 14:13:23 -0700

    Add workshop_bundler.sh, for making a 1.0/1.1 compatible zip

---
+++ workshop_bundler.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p Multiplayer
+cp -r About Languages Multiplayer/
+
+rm -rf Multiplayer/1.1
+mkdir -p Multiplayer/1.1
+cp -r Assemblies Defs Multiplayer/1.1/
+
+mkdir -p Multiplayer/1.0
+git --work-tree=Multiplayer/1.0 checkout origin/rw-1.0 -- Assemblies Defs
+git reset Assemblies Defs
+
+rm -f Multiplayer.zip
+zip -r -q Multiplayer.zip Multiplayer
+
+echo "Ok, $PWD/Multiplayer.zip ready for uploading to Workshop"
