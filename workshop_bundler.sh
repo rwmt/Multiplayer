5d8d95fe81250916dc87bb7fa49b632d2d053419
Nebual <ben1120@gmail.com>
2020-04-06 01:41:51 -0700

    workshop_bundler.sh: copy Version.cs into About.xml

--- workshop_bundler.sh
+++ workshop_bundler.sh
@@ -3,6 +3,7 @@
 mkdir -p Multiplayer
 cp -r About Languages Multiplayer/
 sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.0</li>" Multiplayer/About/About.xml
+sed -i "s/This is version .*\$/This is version $(grep -Po '(?<=Version = ")[0-9\.]+' Source/Common/Version.cs)./" Multiplayer/About/About.xml
 
 rm -rf Multiplayer/1.1
 mkdir -p Multiplayer/1.1
