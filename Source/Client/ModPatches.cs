3ec658b04057c09f44f80447e452bd3f9449879c
Nebual <ben1120@gmail.com>
2020-04-17 17:01:42 -0700

    Mod Compatibility: replace 'hold shift' with setting (default on)

--- Source/Client/ModPatches.cs
+++ Source/Client/ModPatches.cs
@@ -114,7 +114,7 @@ namespace Multiplayer.Client
 
         public static void ModManager_ButtonPrefix(object __instance, ModMetaData ____selected, Rect rect, Dictionary<string, string> ____modNameTruncationCache)
         {
-            if (!Input.GetKey(KeyCode.LeftShift)) return;
+            if (!MultiplayerMod.settings.showModCompatibility) return;
 
             var tooltip = "";
             var mod = ____selected;
