dafa3116eec81368d605093a3b283d93b5fb38bb
notfood <notfood@users.noreply.github.com>
2020-05-22 00:55:23 -0500

    Update to 1.1.2647

--- Source/Client/Sync/SyncHandlers.cs
+++ Source/Client/Sync/SyncHandlers.cs
@@ -827,13 +827,13 @@ namespace Multiplayer.Client
 
             SyncMethod.Register(typeof(CompFlickable), "<CompGetGizmosExtra>b__20_1"); // Toggle flick designation
             SyncMethod.Register(typeof(Pawn_PlayerSettings), "<GetGizmos>b__31_1");    // Toggle release animals
-            SyncMethod.Register(typeof(Building_TurretGun), "<GetGizmos>b__58_2");     // Toggle turret hold fire
+            SyncMethod.Register(typeof(Building_TurretGun), "<GetGizmos>b__59_2");     // Toggle turret hold fire
             SyncMethod.Register(typeof(Building_Trap), "<GetGizmos>b__23_1");          // Toggle trap auto-rearm
             SyncMethod.Register(typeof(Building_Door), "<GetGizmos>b__57_1");          // Toggle door hold open
             SyncMethod.Register(typeof(Zone_Growing), "<GetGizmos>b__13_1");           // Toggle zone allow sow
 
             SyncMethod.Register(typeof(PriorityWork), "<GetGizmos>b__17_0");                // Clear prioritized work
-            SyncMethod.Register(typeof(Building_TurretGun), "<GetGizmos>b__58_1");          // Reset forced target
+            SyncMethod.Register(typeof(Building_TurretGun), "<GetGizmos>b__59_1");          // Reset forced target
             SyncMethod.Register(typeof(UnfinishedThing), "<GetGizmos>b__27_0");             // Cancel unfinished thing
             SyncMethod.Register(typeof(CompTempControl), "<CompGetGizmosExtra>b__8_2");     // Reset temperature
 
@@ -859,9 +859,9 @@ namespace Multiplayer.Client
 
             SyncMethod.Register(typeof(CompShuttle), "<CompGetGizmosExtra>b__39_1"); // Toggle autoload
             SyncMethod.Register(typeof(CompShuttle), "<CompGetGizmosExtra>b__39_2"); // Send shuttle
-            SyncMethod.Register(typeof(MonumentMarker), "<GetGizmos>b__27_1"); // Build Monument Quest - Monument Marker: cancel/remove marker
-            SyncDelegate.Register(typeof(MonumentMarker), "<>c__DisplayClass27_0", "<GetGizmos>b__3"); // Build Monument Quest - Monument Marker: place blueprints
-            SyncMethod.Register(typeof(MonumentMarker), "<GetGizmos>b__27_4"); // Build Monument Quest - Monument Marker: dev build all
+            SyncMethod.Register(typeof(MonumentMarker), "<GetGizmos>b__28_1"); // Build Monument Quest - Monument Marker: cancel/remove marker
+            SyncDelegate.Register(typeof(MonumentMarker), "<>c__DisplayClass28_0", "<GetGizmos>b__3"); // Build Monument Quest - Monument Marker: place blueprints
+            SyncMethod.Register(typeof(MonumentMarker), "<GetGizmos>b__28_4"); // Build Monument Quest - Monument Marker: dev build all
 
             SyncDelegate.Register(typeof(ITab_ContentsTransporter), "<>c__DisplayClass7_0", "<DoItemsLists>b__0").SetContext(SyncContext.MapSelected); // Discard loaded thing
         }
