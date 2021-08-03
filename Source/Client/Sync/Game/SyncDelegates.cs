a3ce32d14d22aa78695c3e72cd698d5e7f78bca1
SokyranTheDragon <exploder1531@gmail.com>
2021-08-03 23:41:05 +0200

    Updated CompTreeConnection synced methods to RW 1.3.3080

--- Source/Client/Sync/Game/SyncDelegates.cs
+++ Source/Client/Sync/Game/SyncDelegates.cs
@@ -75,9 +75,9 @@ namespace Multiplayer.Client
 
             SyncDelegate.Register(typeof(CompPlantable), "<>c__DisplayClass9_0", "<BeginTargeting>b__3");    // Select cell to plant to after confirmation
             SyncMethod.Register(typeof(CompPlantable), "<CompGetGizmosExtra>b__8_1");                        // Cancel planting
-            SyncMethod.Register(typeof(CompTreeConnection), "<CompGetGizmosExtra>b__72_1").SetDebugOnly();   // Spawn dryad
-            SyncMethod.Register(typeof(CompTreeConnection), "<CompGetGizmosExtra>b__72_2").SetDebugOnly();   // Increase connection strength by 10%
-            SyncMethod.Register(typeof(CompTreeConnection), "<CompGetGizmosExtra>b__72_3").SetDebugOnly();   // Decrease connection strength by 10%
+            SyncMethod.Register(typeof(CompTreeConnection), "<CompGetGizmosExtra>b__73_1").SetDebugOnly();   // Spawn dryad
+            SyncMethod.Register(typeof(CompTreeConnection), "<CompGetGizmosExtra>b__73_2").SetDebugOnly();   // Increase connection strength by 10%
+            SyncMethod.Register(typeof(CompTreeConnection), "<CompGetGizmosExtra>b__73_3").SetDebugOnly();   // Decrease connection strength by 10%
 
             // (Un)assigning ideology roles
             SyncDelegate.Register(typeof(SocialCardUtility), "<>c__DisplayClass28_0", "<DrawPawnRole>b__2"); // Unnasign role from a pawn
