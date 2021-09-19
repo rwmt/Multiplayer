7a266c682cce61d238fa407f25ec716bb20a1e48
Zetrith <zetrith@gmail.com>
2021-09-19 18:38:34 +0200

    workshop_bundler.sh changes

--- workshop_bundler.sh
+++ workshop_bundler.sh
@@ -5,8 +5,9 @@ VERSION=$(grep -Po '(?<=Version = ")[0-9\.]+' Source/Common/Version.cs)
 git submodule update --init --recursive
 
 mkdir -p Multiplayer
-cp -r About Languages Multiplayer/
+cp -r About Multiplayer/
 rm -rf Multiplayer/Languages/.git Multiplayer/Languages/LICENSE Multiplayer/Languages/README.md
+
 sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.2</li>" Multiplayer/About/About.xml
 sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.1</li>" Multiplayer/About/About.xml
 sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.0</li>" Multiplayer/About/About.xml
@@ -15,19 +16,19 @@ sed -i "s/<version>.*<\/version>\$/<version>${VERSION}<\/version>/" Multiplayer/
 
 rm -rf Multiplayer/1.3
 mkdir -p Multiplayer/1.3
-cp -r Assemblies Defs Multiplayer/1.3/
+cp -r Assemblies Defs Languages Multiplayer/1.3/
 
 mkdir -p Multiplayer/1.2
-git --work-tree=Multiplayer/1.2 checkout origin/rw-1.2 -- Assemblies Defs
-git reset Assemblies Defs
+git --work-tree=Multiplayer/1.2 checkout origin/rw-1.2 -- Assemblies Defs Languages
+git reset Assemblies Defs Languages
 
 mkdir -p Multiplayer/1.1
-git --work-tree=Multiplayer/1.1 checkout origin/rw-1.1 -- Assemblies Defs
-git reset Assemblies Defs
+git --work-tree=Multiplayer/1.1 checkout origin/rw-1.1 -- Assemblies Defs Languages
+git reset Assemblies Defs Languages
 
 mkdir -p Multiplayer/1.0
-git --work-tree=Multiplayer/1.0 checkout origin/rw-1.0 -- Assemblies Defs
-git reset Assemblies Defs
+git --work-tree=Multiplayer/1.0 checkout origin/rw-1.0 -- Assemblies Defs Languages
+git reset Assemblies Defs Languages
 
 rm -f Multiplayer.zip
 zip -r -q Multiplayer.zip Multiplayer
