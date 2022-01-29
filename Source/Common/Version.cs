e77d9a499bbc9af5e930e4196d4795e8121f0787
notfood <notfood@users.noreply.github.com>
2022-01-28 21:43:38 -0500

    Version 0.6.1.0

--- Source/Common/Version.cs
+++ Source/Common/Version.cs
@@ -2,7 +2,7 @@ namespace Multiplayer.Common
 {
     public static class MpVersion
     {
-        public const string Version = "0.6.0.6";
+        public const string Version = "0.6.1.0";
         public const int Protocol = 25;
 
         public const string ApiAssemblyName = "0MultiplayerAPI";
