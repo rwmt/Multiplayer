e17201e15a9e0057852d2156e14687eccfaf7de6
notfood <notfood@users.noreply.github.com>
2022-04-06 04:06:12 -0500

    Add 1.3.3311 support

--- workshop_bundler.sh
+++ workshop_bundler.sh
@@ -10,6 +10,31 @@ git submodule update --init --recursive
 
 mkdir -p Multiplayer
 cp -r About Textures Multiplayer/
+cat <<EOF > Multiplayer/LoadFolders.xml
+<loadFolders>
+  <v1.3.3311>
+    <li>/</li>
+    <li>1.3-1</li>
+  </v1.3.3311>
+  <v1.3>
+    <li>/</li>
+    <li>1.3-0</li>
+  </v1.3>
+  <v1.2>
+    <li>/</li>
+    <li>1.2</li>
+  </v1.2>
+  <v1.1>
+    <li>/</li>
+    <li>1.1</li>
+  </v1.1>
+  <v1.1>
+    <li>/</li>
+    <li>1.0</li>
+  </v1.1>
+</loadFolders>
+EOF
+
 
 sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.2</li>" Multiplayer/About/About.xml
 sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.1</li>" Multiplayer/About/About.xml
@@ -21,6 +46,13 @@ rm -rf Multiplayer/1.3
 mkdir -p Multiplayer/1.3
 cp -r Assemblies Defs Languages Multiplayer/1.3/
 rm -f Multiplayer/1.3/Languages/.git Multiplayer/1.3/Languages/LICENSE Multiplayer/1.3/Languages/README.md
+mv Multiplayer/1.3 Multiplayer/1.3-1
+
+mkdir -p Multiplayer/1.3
+git --work-tree=Multiplayer/1.3 checkout origin/rw-1.3 -- Assemblies Defs
+cp -r Languages Multiplayer/1.3/
+git reset Assemblies Defs Languages
+mv Multiplayer/1.3 Multiplayer/1.3-0
 
 mkdir -p Multiplayer/1.2
 git --work-tree=Multiplayer/1.2 checkout origin/rw-1.2 -- Assemblies Defs Languages
