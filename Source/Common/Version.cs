c8adbd7e661c243cb17aa712104d3dc125bd4671
Zetrith <zetrith@gmail.com>
2022-11-05 00:17:16 +0100

    Version 0.7, update Languages

--- Source/Common/Version.cs
+++ Source/Common/Version.cs
@@ -2,7 +2,7 @@ namespace Multiplayer.Common
 {
     public static class MpVersion
     {
-        public const string Version = "0.6.2.0";
+        public const string Version = "0.7";
         public const int Protocol = 28;
 
         public const string ApiAssemblyName = "0MultiplayerAPI";
