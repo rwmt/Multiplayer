a8e581fc2ba382c320f01e46a9cd6efbea78469c
zetrith <zetrith@gmail.com>
2022-11-06 21:37:14 +0100

    Workshop bundle action, fix bundled 1.3

--- workshop_bundler.sh
+++ workshop_bundler.sh
@@ -2,19 +2,20 @@
 
 VERSION=$(grep -Po '(?<=Version = ")[0-9\.]+' Source/Common/Version.cs)
 
+git submodule update --init --recursive
+
 cd Source
 dotnet build --configuration Release
 cd ..
 
-git submodule update --init --recursive
-
 rm -rf Multiplayer/
 mkdir -p Multiplayer
+cd Multiplayer
 
 # About/ and Textures/ are shared between all versions
-cp -r About Textures Multiplayer/
+cp -r ../About ../Textures .
 
-cat <<EOF > Multiplayer/LoadFolders.xml
+cat <<EOF > LoadFolders.xml
 <loadFolders>
   <v1.4>
     <li>/</li>
@@ -22,8 +23,12 @@ cat <<EOF > Multiplayer/LoadFolders.xml
   </v1.4>
   <v1.3.3311>
     <li>/</li>
-    <li>1.3</li>
+    <li>1.3.3311</li>
   </v1.3.3311>
+  <v1.3>
+    <li>/</li>
+    <li>1.3</li>
+  </v1.3>
   <v1.2>
     <li>/</li>
     <li>1.2</li>
@@ -32,25 +37,32 @@ cat <<EOF > Multiplayer/LoadFolders.xml
 EOF
 
 
-sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.3</li>" Multiplayer/About/About.xml
-sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.2</li>" Multiplayer/About/About.xml
-sed -i "/Multiplayer mod for RimWorld./aThis is version ${VERSION}." Multiplayer/About/About.xml
-sed -i "s/<version>.*<\/version>\$/<version>${VERSION}<\/version>/" Multiplayer/About/Manifest.xml
+sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.3</li>" About/About.xml
+sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.2</li>" About/About.xml
+sed -i "/Multiplayer mod for RimWorld./aThis is version ${VERSION}." About/About.xml
+sed -i "s/<version>.*<\/version>\$/<version>${VERSION}<\/version>/" About/Manifest.xml
 
 # The current version
-rm -rf Multiplayer/1.4
-mkdir -p Multiplayer/1.4
-cp -r Assemblies Defs Languages Multiplayer/1.4/
-rm -f Multiplayer/1.4/Languages/.git Multiplayer/1.4/Languages/LICENSE Multiplayer/1.4/Languages/README.md
+mkdir -p 1.4
+cp -r ../Assemblies ../Defs ../Languages 1.4/
+rm -f 1.4/Languages/.git 1.4/Languages/LICENSE 1.4/Languages/README.md
 
 # Past versions
-mkdir -p Multiplayer/1.3
-git --work-tree=Multiplayer/1.3 checkout origin/rw-1.3.3311 -- Assemblies Defs Languages
+mkdir -p 1.3.3311
+git --work-tree=1.3.3311 checkout --recurse-submodules origin/rw-1.3.3311 -- Assemblies Defs Languages
+git reset Assemblies Defs Languages
+rm -f 1.3.3311/Languages/.git 1.3.3311/Languages/LICENSE 1.3.3311/Languages/README.md
+
+mkdir -p 1.3
+git --work-tree=1.3 checkout --recurse-submodules origin/rw-1.3 -- Assemblies Defs Languages
+git reset Assemblies Defs Languages
+rm -f 1.3/Languages/.git 1.3/Languages/LICENSE 1.3/Languages/README.md
+
+mkdir -p 1.2
+git --work-tree=1.2 checkout origin/rw-1.2 -- Assemblies Defs Languages
 git reset Assemblies Defs Languages
 
-mkdir -p Multiplayer/1.2
-git --work-tree=Multiplayer/1.2 checkout origin/rw-1.2 -- Assemblies Defs Languages
-git reset Assemblies Defs Languages
+cd ..
 
 # Zip for Github releases
 rm -f Multiplayer-v$VERSION.zip
