a2c4b39a8fcc9bbc81b0a05c596c85990de73d5f
Zetrith <zetrith@gmail.com>
2022-11-06 01:54:12 +0100

    Update workshop_bundler.sh, stop bundling 1.0 and 1.1, update Languages

--- workshop_bundler.sh
+++ workshop_bundler.sh
@@ -8,64 +8,51 @@ cd ..
 
 git submodule update --init --recursive
 
+rm -rf Multiplayer/
 mkdir -p Multiplayer
+
+# About/ and Textures/ are shared between all versions
 cp -r About Textures Multiplayer/
+
 cat <<EOF > Multiplayer/LoadFolders.xml
 <loadFolders>
+  <v1.4>
+    <li>/</li>
+    <li>1.4</li>
+  </v1.4>
   <v1.3.3311>
     <li>/</li>
-    <li>1.3-1</li>
+    <li>1.3</li>
   </v1.3.3311>
-  <v1.3>
-    <li>/</li>
-    <li>1.3-0</li>
-  </v1.3>
   <v1.2>
     <li>/</li>
     <li>1.2</li>
   </v1.2>
-  <v1.1>
-    <li>/</li>
-    <li>1.1</li>
-  </v1.1>
-  <v1.1>
-    <li>/</li>
-    <li>1.0</li>
-  </v1.1>
 </loadFolders>
 EOF
 
 
+sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.3</li>" Multiplayer/About/About.xml
 sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.2</li>" Multiplayer/About/About.xml
-sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.1</li>" Multiplayer/About/About.xml
-sed -i "/<supportedVersions>/ a \ \ \ \ <li>1.0</li>" Multiplayer/About/About.xml
 sed -i "/Multiplayer mod for RimWorld./aThis is version ${VERSION}." Multiplayer/About/About.xml
 sed -i "s/<version>.*<\/version>\$/<version>${VERSION}<\/version>/" Multiplayer/About/Manifest.xml
 
-rm -rf Multiplayer/1.3
-mkdir -p Multiplayer/1.3
-cp -r Assemblies Defs Languages Multiplayer/1.3/
-rm -f Multiplayer/1.3/Languages/.git Multiplayer/1.3/Languages/LICENSE Multiplayer/1.3/Languages/README.md
-mv Multiplayer/1.3 Multiplayer/1.3-1
+# The current version
+rm -rf Multiplayer/1.4
+mkdir -p Multiplayer/1.4
+cp -r Assemblies Defs Languages Multiplayer/1.4/
+rm -f Multiplayer/1.4/Languages/.git Multiplayer/1.4/Languages/LICENSE Multiplayer/1.4/Languages/README.md
 
+# Past versions
 mkdir -p Multiplayer/1.3
-git --work-tree=Multiplayer/1.3 checkout origin/rw-1.3 -- Assemblies Defs
-cp -r Languages Multiplayer/1.3/
+git --work-tree=Multiplayer/1.3 checkout origin/rw-1.3.3311 -- Assemblies Defs Languages
 git reset Assemblies Defs Languages
-mv Multiplayer/1.3 Multiplayer/1.3-0
 
 mkdir -p Multiplayer/1.2
 git --work-tree=Multiplayer/1.2 checkout origin/rw-1.2 -- Assemblies Defs Languages
 git reset Assemblies Defs Languages
 
-mkdir -p Multiplayer/1.1
-git --work-tree=Multiplayer/1.1 checkout origin/rw-1.1 -- Assemblies Defs Languages
-git reset Assemblies Defs Languages
-
-mkdir -p Multiplayer/1.0
-git --work-tree=Multiplayer/1.0 checkout origin/rw-1.0 -- Assemblies Defs Languages
-git reset Assemblies Defs Languages
-
+# Zip for Github releases
 rm -f Multiplayer-v$VERSION.zip
 zip -r -q Multiplayer-v$VERSION.zip Multiplayer
 
