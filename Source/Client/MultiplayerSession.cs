7808a9a6f2364003c97c175888876f89a81fbc1c
Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>
2023-06-20 01:01:14 +0200

    Include using for API

--- Source/Client/MultiplayerSession.cs
+++ Source/Client/MultiplayerSession.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using Multiplayer.API;
 using Multiplayer.Client.Util;
 using UnityEngine;
 using Verse;
