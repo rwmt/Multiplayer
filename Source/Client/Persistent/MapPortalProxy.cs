34f5fedc83455891864da9a2814a6cc0af3b3105
SokyranTheDragon <exploder1531@gmail.com>
2024-05-09 17:15:35 +0200

    Added session for map portals

---
+++ Source/Client/Persistent/MapPortalProxy.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using UnityEngine;
+
+namespace Multiplayer.Client.Persistent;
+
+public class MapPortalProxy(MapPortal portal) : Dialog_EnterPortal(portal), ISwitchToMap
+{
+    public static MapPortalProxy drawing;
+    public bool itemsReady = false;
+
+    public MapPortalSession Session => portal.Map.MpComp().sessionManager.GetFirstOfType<MapPortalSession>();
+
+    public override void DoWindowContents(Rect inRect)
+    {
+        drawing = this;
+        var session = Session;
+        SyncSessionWithTransferablesMarker.DrawnSessionWithTransferables = session;
+
+        try
+        {
+            if (session == null)
+                Close();
+
+            base.DoWindowContents(inRect);
+        }
+        finally
+        {
+            drawing = null;
+            SyncSessionWithTransferablesMarker.DrawnSessionWithTransferables = null;
+        }
+    }
+}
