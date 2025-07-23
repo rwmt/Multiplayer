07013307dd2508d69f9d6247443cf6ad79341da3
Tick-git <62646477+Tick-git@users.noreply.github.com>
2025-07-23 23:28:27 +0200

    Fix visual ThrowLavaSmoke desync by swapping CurrentMap
    The `ThrowLavaSmoke ` method spawns a small visual effect. This method is guarded by `ShouldSpawnMotesAt`.
    
    `ShouldSpawnMotesAt` uses `Find.CurrentMap` and compares it to the provided map. In multi-faction gameplay - and possibly in multi-map gameplay - `CurrentMap` is not updated while ticking.
    
    Therefore, for the map that is not spawning the visual effect, the comparison returns `false`, while on the other map it returns `true`, leading to a desync.
    
    In this commit, `CurrentMap` is temporarily swapped to the target map within the `ThrowLavaSmoke ` call to ensure proper behavior.

--- Source/Client/Patches/OdysseyMultifaction.cs
+++ Source/Client/Patches/OdysseyMultifaction.cs
@@ -1,11 +1,19 @@
 using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
 using Verse;
 
 namespace Multiplayer.Client.Patches
 {
-    [HarmonyPatch(typeof(FishShadowComponent), nameof(FishShadowComponent.SpawnFishFleck))]
+    [HarmonyPatch]
     internal static class Patch_FishShadowComponent_MapComponentTick
     {
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return AccessTools.Method(typeof(FishShadowComponent), nameof(FishShadowComponent.SpawnFishFleck));
+            yield return AccessTools.Method(typeof(LavaFXComponent), nameof(LavaFXComponent.ThrowLavaSmoke));
+        }
+
         static void Prefix(Map map, ref sbyte __state)
         {
             if (Multiplayer.Client == null) return;
