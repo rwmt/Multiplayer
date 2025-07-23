cfca1ea3043441cb967f2b9530b55d887225ad9d
Tick-git <62646477+Tick-git@users.noreply.github.com>
2025-07-24 00:03:15 +0200

    Fix visual GlowForest desync by swapping CurrentMap
    Fix visual Glowforest desync by swapping CurrentMap
    The `EmissionTick` method spawns a small visual effect. This process is guarded by `ShouldSpawnMotesAt`.
    
    `ShouldSpawnMotesAt` uses `Find.CurrentMap` and compares it to the provided map. In multi-faction gameplay - and possibly in multi-map gameplay - `CurrentMap` is not updated while ticking.
    
    Therefore, for the map that is not spawning the visual effect, the comparison returns `false`, while on the other map it returns `true`, leading to a desync.
    
    In this commit, `CurrentMap` is temporarily swapped to the target map around the `ShouldSpawnMotesAt` call to ensure proper behavior.

--- Source/Client/Patches/OdysseyMultifaction.cs
+++ Source/Client/Patches/OdysseyMultifaction.cs
@@ -1,12 +1,14 @@
 using HarmonyLib;
+using RimWorld;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
 using Verse;
 
 namespace Multiplayer.Client.Patches
 {
     [HarmonyPatch]
-    internal static class Patch_FishShadowComponent_MapComponentTick
+    internal static class PatchVisualEffectsUsingShouldSpawnMotesAt
     {
         static IEnumerable<MethodBase> TargetMethods()
         {
@@ -27,6 +29,47 @@ namespace Multiplayer.Client.Patches
             if (Multiplayer.Client == null) return;
 
             Current.Game.currentMapIndex = __state;
+        }
+    }
+
+    [HarmonyPatch(typeof(CompFleckEmitterLongTerm), nameof(CompFleckEmitterLongTerm.EmissionTick))]
+    static class PatchEmissionTickShouldSpawnMotesAt
+    {
+        static MethodInfo OriginalMethod = AccessTools.Method(typeof(GenView), nameof(GenView.ShouldSpawnMotesAt),[typeof(IntVec3), typeof(Map), typeof(bool)]);
+        static MethodInfo ReplacementMethod = AccessTools.Method(typeof(PatchEmissionTickShouldSpawnMotesAt), nameof(ShouldSpawnMotesAtReplacement));
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            MethodInfo changed = AccessTools.Method(typeof(PatchEmissionTickShouldSpawnMotesAt), nameof(ShouldSpawnMotesAtReplacement));
+
+            foreach (var ins in instructions)
+            {
+                if (ins.Calls(OriginalMethod))
+                    yield return new CodeInstruction(OpCodes.Call, changed);
+                else
+                    yield return ins;
+            }
+        }
+
+        public static bool ShouldSpawnMotesAtReplacement(IntVec3 cell, Map map, bool flag)
+        {
+            if (Multiplayer.Client == null)
+                return cell.ShouldSpawnMotesAt(map, flag);
+
+            bool shouldSpawn;
+            sbyte prevMapIndex = Current.Game.currentMapIndex;
+            Current.Game.currentMapIndex = (sbyte)map.Index;
+
+            try
+            {
+                shouldSpawn = cell.ShouldSpawnMotesAt(map, flag);
+            }
+            finally
+            {
+                Current.Game.currentMapIndex = prevMapIndex;
+            }
+
+            return shouldSpawn;
         }
     }
 }
