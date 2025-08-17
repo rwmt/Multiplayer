83f6835865e4c42ebb9af36119ed8931f285c9e1
Tick-git <62646477+Tick-git@users.noreply.github.com>
2025-08-17 03:26:09 +0200

    Clean up

--- Source/Client/Saving/ThingsById.cs
+++ Source/Client/Saving/ThingsById.cs
@@ -8,23 +8,16 @@ public class ThingsById
 {
     private Dictionary<int, Thing> thingsById = new();
 
-    public void Register(Thing t)
-    {
-        thingsById[t.thingIDNumber] = t;
-    }
+    public void Register(Thing t) => thingsById[t.thingIDNumber] = t;
 
-    public void Unregister(Thing t)
-    {
-        thingsById.Remove(t.thingIDNumber);
-    }
+    public void Unregister(Thing t) => thingsById.Remove(t.thingIDNumber);
 
-    public void UnregisterAllFrom(Map map)
-    {
-        thingsById.RemoveAll(kv => kv.Value.Map == map);
-    }
+    public void UnregisterAllFrom(Map map) => thingsById.RemoveAll(kv => kv.Value.Map == map);
 
     public Thing GetValueSafe(int thingIDNumber) => thingsById.GetValueSafe(thingIDNumber);
+
     public Thing GetValue(int thingIdNumber) => thingsById[thingIdNumber];
+
     public bool TryGetValue(int thingIDNumber, out Thing thing) => thingsById.TryGetValue(thingIDNumber, out thing);
 
 }
