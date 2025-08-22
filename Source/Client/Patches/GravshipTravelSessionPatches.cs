2d63da5b7d56d59ee5de62c5b1cc201b787ea33b
Tick-git <62646477+Tick-git@users.noreply.github.com>
2025-08-22 23:52:14 +0200

    Revert "Add Guard Against Patch Usage In Singleplayer"
    This reverts commit 425519adb206fb89a9de9d53cd1caea62d21b619.

--- Source/Client/Patches/GravshipTravelSessionPatches.cs
+++ Source/Client/Patches/GravshipTravelSessionPatches.cs
@@ -203,8 +203,6 @@ namespace Multiplayer.Client.Patches
         }
         static void Postfix(Gravship gravship)
         {
-            if (Multiplayer.Client == null) return;
-
             GravshipTravelUtils.OpenSessionAt(gravship.destinationTile);
         }
     }
