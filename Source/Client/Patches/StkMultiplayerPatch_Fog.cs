f1c8fde56e3620c57b330e374f2eae45db80125f
still222 <118853487+still222@users.noreply.github.com>
2025-09-10 18:19:38 +0500

    Update StkMultiplayerPatch_Fog.cs
    Updated the patch with a more in-line with the rest version, suggested by Tick.
    Tested it a little, looks like its working fine.

--- Source/Client/Patches/StkMultiplayerPatch_Fog.cs
+++ Source/Client/Patches/StkMultiplayerPatch_Fog.cs
@@ -1,32 +1,19 @@
 using HarmonyLib;
 using Verse;
 using RimWorld;
-using Multiplayer.API;
 
 namespace Multiplayer.Client;
 
 [HarmonyPatch(typeof(FogGrid), nameof(FogGrid.Notify_PawnEnteringDoor))]
-public static class Patch_FogGrid_Notify_PawnEnteringDoor
+public static class Patch_FogGrid_Faction
 {
-	public static bool Prefix(FogGrid __instance, Building_Door door, Pawn pawn)
+	static void Prefix(FogGrid __instance, Building_Door door, Pawn pawn)
 	{
-		if (!MP.IsInMultiplayer) return true;
-
-		if (IsPlayerControlledFaction(pawn.Faction) || IsPlayerControlledFaction(pawn.HostFaction))
-			__instance.FloodUnfogAdjacent(door.Position, false);
-
-		return false;
+		FactionContext.Push(pawn.Faction);
 	}
 
-	private static bool IsPlayerControlledFaction(Faction faction)
+	static void Finalizer()
 	{
-		if (faction == null) return false;
-
-		// Vanilla single-player
-		if (faction == Faction.OfPlayer) return true;
-
-		// Multiplayer: works for the original host faction FOR SURE, need more testing for clients
-		return faction.IsPlayer;
+		FactionContext.Pop();
 	}
-
 }
