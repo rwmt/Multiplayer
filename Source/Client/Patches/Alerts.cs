80c657c45d05ccd0a5edae21a144ee1111cb20a1
still222 <118853487+still222@users.noreply.github.com>
2025-09-13 02:24:48 +0500

    Update Alerts.cs
    Added fix for multifaction "Abandoned baby" warning.
    
    It triggered for incorrect players, usually during breastfed.

--- Source/Client/Patches/Alerts.cs
+++ Source/Client/Patches/Alerts.cs
@@ -32,3 +32,18 @@ public static class Patch_Alert_SlavesUnsuppressed_Targets
 		__result = __result.Where(pawn => pawn.Faction == Faction.OfPlayer).ToList();
 	}
 }
+
+// Fixing "Abandoned baby" warning , often appearing for other players babies, when they are beign fed. 
+// You will not get abandoned warning: (baby from other faction without the same faction adult)
+// for (other) player owned babies at all, unless it's happened directly on YOUR base.
+// Example: someone catapulted their baby on your map. Your own babies get a different set of warning, which works fine.
+[HarmonyPatch(typeof(Alert_AbandonedBaby), "AbandonedBabies")]
+public static class Patch_Alert_AbandonedBaby_MultifactionWarning
+{
+	public static void Postfix(ref List<Pawn> __result)
+	{
+		if (!MP.IsInMultiplayer) return;
+
+		__result = __result.Where(pawn => !pawn.Faction.IsPlayer || pawn.MapHeld.ParentFaction == Faction.OfPlayer).ToList();
+	}
+}
