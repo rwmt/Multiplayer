b2c003a0fffa6cc33bf736236d30a3de5347c38a
mibac138 <5672750+mibac138@users.noreply.github.com>
2025-09-17 23:19:13 +0200

    Add Git commit sha information

--- Source/Common/Version.cs
+++ Source/Common/Version.cs
@@ -1,9 +1,18 @@
+using System.Linq;
+using System.Reflection;
+
 namespace Multiplayer.Common
 {
     public static class MpVersion
     {
         public const string Version = "0.11.0";
         public const int Protocol = 49;
+
+        public static readonly string? GitHash = Assembly.GetExecutingAssembly()
+            .GetCustomAttributes<AssemblyMetadataAttribute>()
+            .FirstOrDefault(attr => attr.Key == "GitHash")?.Value;
+
+        public static readonly string VersionWithHash = Version + (GitHash != null ? $"+{GitHash}" : "");
 
         public const string ApiAssemblyName = "0MultiplayerAPI";
 
