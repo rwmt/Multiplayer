a09b9006c76052bb95b31f8d29e4a772d2ef9c0a
Sakura-TA <Sakura_TA@163.com>
2026-03-25 03:22:24 +0800

    using __state instead of local variable

--- Source/Client/Patches/AreaSource.cs
+++ Source/Client/Patches/AreaSource.cs
@@ -11,26 +11,23 @@ namespace Multiplayer.Client.Patches
     [HarmonyPatch(typeof(AreaSource))]
     static class AreaSource_Patch
     {
-        static AreaManager old_manager;
-
         [HarmonyPatch(nameof(AreaSource.ComputeAll))]
         [HarmonyPatch(nameof(AreaSource.UpdateIncrementally))]
-        static void Prefix(AreaSource __instance)
+        static void Prefix(AreaSource __instance, ref AreaManager __state)
         {
             if (Multiplayer.Client == null || !Multiplayer.GameComp.multifaction) return;
-            old_manager = __instance.map.areaManager;
+            __state = __instance.map.areaManager;
             __instance.map.areaManager = __instance.map.MpComp().AllAreaManager();
         }
 
         [HarmonyPatch(nameof(AreaSource.ComputeAll))]
         [HarmonyPatch(nameof(AreaSource.UpdateIncrementally))]
-        static void Finalizer(AreaSource __instance)
+        static void Finalizer(AreaSource __instance, AreaManager __state)
         {
             if (Multiplayer.Client == null || !Multiplayer.GameComp.multifaction) return;
 
             // restore original
-            __instance.map.areaManager = old_manager;
-            old_manager = null;
+            __instance.map.areaManager = __state;
         }
     }
 }
